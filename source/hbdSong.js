93155feb3d7cad80a803f23ca6160617c0bf8a31
LinLaePhyu <78365645+LinLaePhyu@users.noreply.github.com>
2024-02-12 13:27:43 +0630

    Add files via upload

---
+++ source/hbdSong.js
@@ -0,0 +1,33 @@
+
+let allMusic = [
+  {
+    name: "Hax - Lucy",
+    artist: "Hax",
+    img: "s1",
+    src: "s1"
+  },
+  {
+    name: "Hax - ချစ်ခဲ့ကြ လမ်းခွဲကြ",
+    artist: "Hax",
+    img: "s2",
+    src: "s2"
+  },
+  {
+    name: "Hax x Nyein Than Thar - မင်းအနားရှိချိန်",
+    artist: "Hax x Nyein Than Thar",
+    img: "s3",
+    src: "s3"
+  },
+  {
+    name: "Hax - အတိတ်မှသည် ပစ္စုပ္ပန်သို့",
+    artist: "Hax",
+    img: "s4",
+    src: "s4"
+  },
+  {
+    name: "Hax - အတိတ်မှသည် ပစ္စုပ္ပန်သို့ EDM",
+    artist: "Hax",
+    img: "s4 (EDM)",
+    src: "s4 (EDM)"
+  },
+];
